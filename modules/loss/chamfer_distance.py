3843c5e144f93fac8fd864caa036ba3e3c5f439e
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2020-09-28 19:35:39 +0800

    calculate the mean of cd loss of all verteices

--- modules/loss/chamfer_distance.py
+++ modules/loss/chamfer_distance.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from config import CD_W1, CD_W2
+from config import CD_W1, CD_W2, BATCH_SIZE
 
 
 class ChamferDistanceLoss(nn.Module):
@@ -8,6 +8,9 @@ class ChamferDistanceLoss(nn.Module):
         super().__init__()
 
     def forward(self, points1: torch.Tensor, points2: torch.Tensor) -> torch.Tensor:
+        self.check_parameters(points1)
+        self.check_parameters(points2)
+
         diff = points1[:, :, None, :] - points2[:, None, :, :]
         dist = torch.sum(diff * diff, dim=3)
         dist1 = dist
@@ -16,4 +19,14 @@ class ChamferDistanceLoss(nn.Module):
         dist_min1, _ = torch.min(dist1, dim=2)
         dist_min2, _ = torch.min(dist2, dim=2)
 
-        return (CD_W1 * torch.sum(dist_min1) + CD_W2 * torch.sum(dist_min2)) / points1.size(0)
+        loss1 = dist_min1.mean()
+        loss2 = dist_min2.mean()
+
+        loss = (CD_W1 * loss1 + CD_W2 * loss2) / BATCH_SIZE
+        return loss
+
+    @staticmethod
+    def check_parameters(points: torch.Tensor):
+        assert points.ndimension() == 3  # (B, N, 3)
+        assert points.size(0) == BATCH_SIZE
+        assert points.size(-1) == 3
