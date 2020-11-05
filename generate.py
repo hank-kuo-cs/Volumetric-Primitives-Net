3a4e4b7406b392905b11ab60a8c203db2d04a0cb
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2020-11-05 19:58:34 +0800

    generate point mixup dataset

--- generate.py
+++ generate.py
@@ -1,10 +1,48 @@
 import os
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
 from modules import ShapeNetDataset
+from modules.augmentation.point_mixup import generate_point_mixup_data
+from modules.transform import rotate_points_forward_x_axis
 from config import *
+
+
+def load_dataset():
+    print('Load dataset...')
+    train_dataset = ShapeNetDataset('train')
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=16)
+    print('Dataset size =', len(train_dataset))
+
+    return train_dataloader
 
 
 if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = DEVICE_NUM
-    print('Load dataset...')
-    train_dataset = ShapeNetDataset('train')
-    train_dataset.save_view_center_dataset()
+    dataset_path = os.path.join(DATASET_ROOT, 'PointMixUp/1')
+    os.makedirs(dataset_path, exist_ok=True)
+
+    dataloader = load_dataset()
+    progress_bar = tqdm(dataloader)
+
+    to_pil = transforms.ToPILImage()
+    n = 1
+
+    for data in progress_bar:
+        rotate_angles = data['rotate_angle'].float().to(DEVICE)
+        view_center_points = data['view_center_points'].to(DEVICE)
+
+        # view_center_points = rotate_points_forward_x_axis(view_center_points, rotate_angles)
+        rgbs, silhouettes, meshes = generate_point_mixup_data(view_center_points)
+
+        for i in range(rgbs.size(0)):
+            rgb = to_pil(rgbs[i].cpu())
+            silhouette = to_pil(silhouettes[i].cpu())
+
+            rgb.save(os.path.join(dataset_path, 'rgb_%d.png' % n))
+            silhouette.save(os.path.join(dataset_path, 'silhouette_%d.png' % n))
+
+            mesh = meshes[i]
+            mesh.save_mesh(os.path.join(dataset_path, 'mesh_%d.obj' % n))
+
+            n += 1
--- modules/augmentation/point_mixup.py
+++ modules/augmentation/point_mixup.py
@@ -57,7 +57,10 @@ def meshes_to_imgs(meshes: list, colors: list) -> (torch.Tensor, torch.Tensor):
     rgbs, silhouettes = [], []
 
     for i in range(len(meshes)):
-        rgb, silhouette, _ = VertexRenderer.render(meshes[i], 1, 0, 0, colors[i][None])
+        p = torch.rand(1).item()
+        rgb, silhouette, _ = VertexRenderer.render(meshes[i], 1, 0, 0, colors[i][None]) if p >= 0.5 \
+            else PhongRenderer.render(meshes[i], 1, 0, 0)
+
         rgbs.append(rgb.permute(0, 3, 1, 2))
         silhouettes.append(silhouette.permute(0, 3, 1, 2))
 
@@ -70,3 +73,14 @@ def meshes_to_imgs(meshes: list, colors: list) -> (torch.Tensor, torch.Tensor):
 def check_parameters(view_center_points: torch.Tensor):
     assert view_center_points.ndimension() == 3  # (B, N, 3)
     assert view_center_points.size(-1) == 3
+
+
+def generate_point_mixup_data(view_center_points: torch.Tensor) -> (torch.Tensor, list):
+    check_parameters(view_center_points)
+
+    mixed_points = mixup_points(view_center_points)
+    recon_meshes, colors = points_to_meshes_and_colors(mixed_points)
+
+    rgbs, silhouettes = meshes_to_imgs(recon_meshes, colors)
+
+    return rgbs, silhouettes, recon_meshes
