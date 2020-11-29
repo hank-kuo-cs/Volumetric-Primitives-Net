fcf821a29ab0d0a0640799a85d8d61bfc6f2a637
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2020-11-29 16:21:11 +0800

    visualize mesh with phong renderer

--- modules/visualize/vp_mesh.py
+++ modules/visualize/vp_mesh.py
@@ -1,8 +1,8 @@
 import torch
 from PIL import Image
 from kaolin.rep import TriangleMesh
-from .render import render, to_pil, concat_pil_image
-from ..augmentation.acd import merge_meshes
+from .render import render, to_pil, concat_pil_image, phong_render
+from ..meshing.convex_decomposition import merge_meshes
 
 COLORS = torch.tensor([[0.0, 0.99, 0.0], [0.0, 0.0, 0.99], [0.99, 0.0, 0.0], [0.6, 0.3, 0.8],
                        [0.99, 0.99, 0.0], [0.0, 0.99, 0.99], [0.99, 0.0, 0.99], [0.6, 0.8, 0.3],
@@ -72,25 +72,18 @@ def compose_vp_meshes(vp_meshes: list):
 
 def visualize_refine_vp_meshes(image: torch.Tensor, vp_meshes: list, save_name: str, predict_vertices: torch.Tensor):
     image = to_pil(image.cpu()).resize((256, 256), Image.BILINEAR)
-    colors = get_colors_of_vps(vp_meshes)
-    mesh = compose_vp_meshes(vp_meshes)
-
-    # union_mesh = merge_meshes(vp_meshes)
-    # union_mesh.cuda()
-    single_colors = torch.full_like(mesh.vertices, fill_value=0.5)[None]
-
-    vertices = mesh.vertices[None]
-    faces = mesh.faces
-    predict_vertices = predict_vertices[None]
+    mesh, uv, texture = merge_meshes(vp_meshes)
+    deformed_mesh = TriangleMesh.from_tensors(predict_vertices, mesh.faces)
 
     gif_imgs = []
-    render_img_direct_pose = render(predict_vertices, faces, colors, 1, 0, 0)
-    for azim in range(0, 360, 30):
-        predict_img = render(predict_vertices, faces, colors, 1, 0, azim)
-        vp_img = render(vertices, faces, colors, 1, 0, azim)
-        single_color_img = render(predict_vertices, faces, single_colors, 1, 0, azim)
+    render_img_direct_pose = phong_render(deformed_mesh, uv, texture, 1, 0, 0)
 
-        imgs = [image, render_img_direct_pose, predict_img, vp_img, single_color_img]
+    for azim in range(0, 360, 30):
+        predict_img = phong_render(deformed_mesh, uv, texture, 1, 0, azim)
+        vp_img = phong_render(mesh, uv, texture, 1, 0, azim)
+        single_color_img = phong_render(deformed_mesh, None, None, 1, 0, azim)
+
+        imgs = [image, render_img_direct_pose, vp_img, predict_img, single_color_img]
         gif_imgs.append(concat_pil_image(imgs))
 
     gif_imgs[0].save(save_name, format='GIF', append_images=gif_imgs[1:], save_all=True, duration=300, loop=0)
