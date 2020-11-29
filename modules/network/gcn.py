074954dcb521e62ed5c23c4f1533ce01691fab28
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2020-11-29 17:39:18 +0800

    fix position encoding vertex dim

--- modules/network/gcn.py
+++ modules/network/gcn.py
@@ -34,10 +34,8 @@ class GCNModel(nn.Module):
         batch_vertices = self.get_batch_vertices(meshes)  # (B, N, 3)
         edge_indices = self.get_edge_indices(meshes[0])  # (2, K)
 
-        if self.use_position_encoding:
-            batch_vertices = self.positional_encoding(batch_vertices)
-
-        x = torch.cat([batch_vertices, img_features[:, None, :].repeat(1, batch_vertices.size(1), 1)], 2)
+        x = self.positional_encoding(batch_vertices) if self.use_position_encoding else batch_vertices
+        x = torch.cat([x, img_features[:, None, :].repeat(1, x.size(1), 1)], 2)
 
         x = self.conv1(x, edge_indices, None)
         x = self.conv2(x, edge_indices, None)
